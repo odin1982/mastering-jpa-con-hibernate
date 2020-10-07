e3c945637868d8e7967f7287a748530fe8d78d46
odinPcWin10 <odin.araujo.barragan@gmail.com>
2020-10-07 00:22:36 -0500

    Mapeando columnas simples con @Column

--- src/org/jpa/entity/Customer.java
+++ src/org/jpa/entity/Customer.java
@@ -1,48 +1,82 @@
 package org.jpa.entity;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="customers")
-//@SequenceGenerator(name = "customer_seq",sequenceName ="customer_seq",initialValue = 1,allocationSize = 1 ) //ORACLE DB
+@Table(name = "customers")
 public class Customer {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	//@GeneratedValue(strategy=GenerationType.SEQUENCE,generator = "customer_seq") //ORACLE DB
-	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
+
+	@Column(name = "first_name", length = 30, nullable = false, updatable = false, unique = true)
 	private String firstName;
+
+	@Column(name = "last_name", length = 50)
 	private String lastName;
-	
+
+	// precision cuantos numeros tendra contando los decimales, scale= cuentos
+	// decimales tendra
+	@Column(name = "salario", precision = 7, scale = 4)
+	private BigDecimal salario;
+
+	// precision cuantos numeros tendra contando los decimales, scale= cuentos
+	// decimales tendra
+	@Column(name = "deudas",scale = 4)
+	private BigDecimal deudas;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public BigDecimal getDeudas() {
+		return deudas;
+	}
+
+	public void setDeudas(BigDecimal deudas) {
+		this.deudas = deudas;
+	}
+
 	@Override
 	public String toString() {
-		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", salario=" + salario
+				+ "]";
 	}
-	
 }
--- src/org/jpa/main/_01/Main_01.java
+++ src/org/jpa/main/_01/Main_01.java
@@ -1,5 +1,7 @@
 package org.jpa.main._01;
 
+import java.math.BigDecimal;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -16,13 +18,35 @@ public class Main_01 {
 		Customer customer = new Customer();
 		customer.setFirstName("Odin");
 		customer.setLastName("Araujo Barragan");
+		customer.setSalario(BigDecimal.valueOf(20.5));
+		customer.setDeudas(BigDecimal.valueOf(1111111111.881234));
 		em.persist(customer);
 
 		Customer customer2 = new Customer();
 		customer2.setFirstName("Amaya");
 		customer2.setLastName("Araujo Hernandez");
+		customer2.setSalario(BigDecimal.valueOf(39.5));
 		em.persist(customer2);
 
+		// prueba de que no permite nombres iguales
+		/*
+		 * Customer customer3 = new Customer(); customer3.setFirstName("Odin");
+		 * customer3.setLastName("Araujo Hernandez"); em.persist(customer3);
+		 */
+
+		// prueba de que no permite nombres nullos
+//		Customer customer4 = new Customer();
+//		customer4.setLastName("Araujo Hernandez");
+//		em.persist(customer4);
+
+		em.getTransaction().commit();
+
+		em.getTransaction().begin();
+		// prueba de que el nombre no se puede actualizar, no genera exception solo no hace el update
+		Customer customer5 = em.find(Customer.class, 1L);
+		customer5.setFirstName("Juanito");
+		customer5.setLastName("Araujo Hernandez");
+		em.persist(customer5);
 		em.getTransaction().commit();
 
 		Customer findObject = em.find(Customer.class, 1L);
